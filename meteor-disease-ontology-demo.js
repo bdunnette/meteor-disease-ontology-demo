ee5d5ef1a582f78f0207170f2857101cf75c760d
Brian Dunnette <brian@dunnette.us>
2015-04-17 16:00:55 -0500

    basic pagination

--- meteor-disease-ontology-demo.js
+++ meteor-disease-ontology-demo.js
@@ -1,18 +1,15 @@
+Pages = new Meteor.Pagination(Diseases);
+
 if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
+  Template.diseases.helpers({
+
   });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
+  Template.diseases.events({
+
   });
 }
 
