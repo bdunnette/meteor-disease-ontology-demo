644cabf2e3ad4c01de6b6364f0dcae6b2ccdacb4
Brian Dunnette <bdunnette@gmail.com>
2015-04-19 14:25:48 -0500

    only download OBO file on startup; still working on the parsing tasks

--- meteor-disease-ontology-demo.js
+++ meteor-disease-ontology-demo.js
@@ -2,6 +2,11 @@ Pages = new Meteor.Pagination(Diseases, {
   perPage: 10,
   sort: {
     name: 1
+  },
+  filters: {
+    oboUrl: {
+      $exists: false
+    }
   }
 });
 
@@ -17,29 +22,16 @@ if (Meteor.isClient) {
 
 if (Meteor.isServer) {
   Meteor.methods({
-    updateDiseases: function() {
-      var diseasesToUpdate = Diseases.find({xrefs: {$exists: false}});
-      console.log(diseasesToUpdate.count() + ' diseases to update');
-      diseasesToUpdate.forEach(function(disease) {
-        var updateCommand = 'Meteor.call("getDisease", "' + disease.id + '");';
-        Queue.add({
-          command: updateCommand
-        });
-      });
-      return diseasesToUpdate.count();
-    }
+
   });
-  Meteor.startup(function () {
-    var diseaseCount = Diseases.find().count();
-    var diseasesToUpdate = Diseases.find({xrefs: {$exists: false}}).count();
-    console.log(diseaseCount + ' diseases in database, ' + diseasesToUpdate + ' with incomplete data');
-    if (diseaseCount === 0) {
-      Queue.add({
-        command: 'Meteor.call("getDisease", "DOID:4");'
-      });
-    }
-    Queue.add({command:'Meteor.call("updateDiseases");'});
-    Queue.setInterval('updateDiseases', 'Meteor.call("updateDiseases");', 600000);
+  Meteor.startup(function() {
+    var oboFile = Diseases.findOne({
+      oboUrl: "http://purl.obolibrary.org/obo/doid.obo"
+    });
+    Queue.add({
+      command: 'Meteor.call("getOBO","http://purl.obolibrary.org/obo/doid.obo");',
+      priority: 1
+    });
     Queue.run();
   });
 }
