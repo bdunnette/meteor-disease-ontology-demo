fe53bf0f3be0010ae44bba3dcf1dc68c9cb8620a
Brian Dunnette <brian@dunnette.us>
2015-05-04 12:38:44 -0500

    update diseases every 5 minutes

--- server/bootstrap.js
+++ server/bootstrap.js
@@ -2,6 +2,6 @@ Meteor.startup(function () {
   var diseaseCount = Diseases.find().count();
   console.log(diseaseCount + ' diseases in database');
   Queue.add({command: 'Meteor.call("updateDiseases");'});
-  Queue.setInterval('updateDiseases', 'Meteor.call("updateDiseases");', 10000);
+  Queue.setInterval('updateDiseases', 'Meteor.call("updateDiseases");', 600000);
   Queue.run();
 });
